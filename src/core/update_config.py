57bf230863206d0efdd93d58737367632515191f
闲鱼管家项目组 <haoyang056@gmail.com>
2026-03-24 14:53:19 +0800

    security: 移除硬编码的 GitHub PAT token

--- src/core/update_config.py
+++ src/core/update_config.py
@@ -4,9 +4,6 @@ import os
 
 GITHUB_OWNER = os.getenv("GITHUB_UPDATE_OWNER", "brianzhibo-design")
 GITHUB_REPO = os.getenv("GITHUB_UPDATE_REPO", "XIANYUGUANJIA")
-GITHUB_TOKEN = os.getenv(
-    "GITHUB_UPDATE_TOKEN",
-    "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",
-)
+GITHUB_TOKEN = os.getenv("GITHUB_UPDATE_TOKEN", "")
 UPDATE_ASSET_SUFFIX = "-update.tar.gz"
 CHECKSUM_ASSET_SUFFIX = "-checksums.txt"
